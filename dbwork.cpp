4e56956ec63e7daf274670746da7e2f2ce264b7c
VorobetsRoman <vorobetsroman@mail.ru>
2018-09-10 18:28:07 +0300

    Added database functions

--- dbwork.cpp
+++ dbwork.cpp
@@ -2,6 +2,8 @@
 #include <QSqlError>
 #include <QSqlDriver>
 #include <QSqlQuery>
+#include <QTime>
+#include <QVariant>
 
 
 
@@ -27,9 +29,13 @@ DbWork::DbWork(QObject *parent) : QObject(parent)
     if (!database->open()) {
         throw "error opening database:" + database->lastError().text();
     } else {
-        /*connect(database->driver(), &QSqlDriver::notification, [=] (QString notification){
-
-        });*/
+        database->driver()->subscribeToNotification(QString("time_to_sex"));
+        connect(database->driver(), static_cast <void(QSqlDriver::*)(const QString&)>(&QSqlDriver::notification),
+                [=] (QString notification) {
+            if (notification == "time_to_sex") {
+                //call Sasha Gray
+            }
+        });
     }
 }
 
@@ -47,10 +53,26 @@ DbWork::~DbWork()
 
 void DbWork::getData()
 {
-    QSqlQuery query(database);
+    QSqlQuery query(*database);
     QString str {"select * from hz"};
-    if (!query.exec) {
+    if (!query.exec()) {
         throw "error executing request" + database->lastError().text();
+    }
+
+    query.prepare("INSERT INTO sasha_gray (partners_size, date)"
+                  "VALUES (:length, :date);");
+    query.bindValue(":length", QString::number(19));
+    query.bindValue(":date", QTime::currentTime().toString(Qt::ISODate));
+
+    if (!query.exec()) {
+        throw "to small!";
+    } else {
+        query.first();
+        do {
+            QString str;
+            str = query.value(0).toString();
+        } while (query.next());
+
     }
 }
 
